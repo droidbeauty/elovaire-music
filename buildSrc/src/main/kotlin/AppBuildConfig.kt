21ba89c628482338417bfda003b21d575f98db36
Droid Beauty <droidbeautycontact@gmail.com>
2026-04-28 21:47:30 +0200

    App build change

--- buildSrc/src/main/kotlin/AppBuildConfig.kt
+++ buildSrc/src/main/kotlin/AppBuildConfig.kt
@@ -2,8 +2,8 @@ object AppBuildConfig {
     const val packageName = "elovaire.music.app"
 
     // Internal build number used by Android for upgrades.
-    const val versionCode = 1000
+    const val versionCode = 1001
 
     // User-facing version shown in the app.
-    const val versionName = "1.0"
+    const val versionName = "1.1"
 }
