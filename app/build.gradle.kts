6538c4c48f811af81d3accdbdf2a2249f27dbb7d
Droid Beauty <droidbeautycontact@gmail.com>
2026-06-30 16:40:15 +0200

    Efficiency fine-tuning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -176,12 +176,10 @@ dependencies {
 
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.animation)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.graphics.path)
 
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.extractor)
