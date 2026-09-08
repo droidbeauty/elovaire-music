6b53fde37c5224c7fd6495318565200078ff4cba
Droid Beauty <droidbeautycontact@gmail.com>
2026-09-08 12:42:31 +0200

    Code cleanup

--- app/src/main/java/com/elovaire/music/data/settings/PersistenceFailure.kt
+++
@@ -1,8 +0,0 @@
-package elovaire.music.droidbeauty.app.data.settings
-
-internal sealed interface PersistenceFailure {
-    data class MigrationFailed(val store: String, val cause: Throwable?) : PersistenceFailure
-    data class CorruptLegacyValue(val key: String) : PersistenceFailure
-    data class ReadFailed(val store: String, val cause: Throwable?) : PersistenceFailure
-    data class WriteFailed(val store: String, val cause: Throwable?) : PersistenceFailure
-}
